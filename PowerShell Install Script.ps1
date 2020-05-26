1f20222450ab7646cff8d089e6ae3ef79815e1a6
CoconutMacaroon <45187468+CoconutMacaroon@users.noreply.github.com>
2020-05-26 16:09:59 -0700

    Update PowerShell Install Script.ps1

--- PowerShell Install Script.ps1
+++ PowerShell Install Script.ps1
@@ -3,13 +3,14 @@ cd appdata\local
 mkdir hangman-game 
 cd hangman-game 
 iwr -outf hangman.zip https://github.com/CoconutMacaroon/Hangman/releases/download/v1.1/Hangman-Win-x64.zip 
-Expand-Archive Hangman.zip 
+Expand-Archive Hangman.zip -Force
 cd Hangman 
 
 
 $WshShell = New-Object -ComObject WScript.Shell 
 $Shortcut = $WshShell.CreateShortcut("$Home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Hangman.lnk") 
 $Shortcut.TargetPath = "$Home\appdata\local\hangman-game\hangman\hangman.exe" 
+$Shortcut.WorkingDirectory = "$Home\appdata\local\hangman-game\hangman\"
 $Shortcut.Save() 
 
 cd .. 
